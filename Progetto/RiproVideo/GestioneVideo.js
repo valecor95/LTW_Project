80678374324b82124817a9a9c978b5aa9d912e5b
Stef97 <sfuscon@live.it>
2018-11-18 23:50:52 +0100

    Ho caricato la mia versione, domani te la spiego bene

---
+++ Progetto/RiproVideo/GestioneVideo.js
@@ -0,0 +1,35 @@
+
+var APIkey = 'AIzaSyDGIo6ylsqkLqOt1skx2gjOeLI57DPaFz8';   //cambia con la tua api
+var maxres = 15;
+var videos = null;
+
+$(document).ready(function(){
+    $('#button1').click(function(){
+        jQuery.ajax({
+                    type: 'GET',
+                    url: "https://www.googleapis.com/youtube/v3/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1",
+                    dataType: 'jsonp',
+                    data: jQuery.extend({q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"), maxResults: maxres, category: 'movie', part: 'snippet', key: APIkey}, { }),
+                    success: function(data){
+                        $(".list-group").empty();
+                        for(var i = 0; i < maxres; i++){
+                            console.log(data.items[i]);
+                            $("<li>" + data.items[i].snippet.title + "" + "<input type='button' id=" + data.items[i].id.videoId + " value='Guarda!' onclick='return PlayVideo(this.id);'>"  + "</li>").appendTo("#results");
+                        }
+                    },
+                    error: function(error){
+                        console.log('errore: ', error);
+                    }
+            });
+    }); 
+});
+        
+function PlayVideo(id){
+    $('#player').css('display','block');
+    var videoID=id;
+    player.loadVideoById({videoId: videoID,
+            startSeconds:0,
+            endSeconds: '',
+            suggestedQuality: 'small'});
+}
+
