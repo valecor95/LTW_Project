0eee726dea2af7f2c1e1dfc88b5c6f1d742afbff
Stef97 <sfuscon@live.it>
2018-12-04 23:38:19 +0100

    Pagina Audio completata, ci saranno eventualmente ritocchi

--- MultimediaSite/paginaAudio/audio.js
+++ MultimediaSite/paginaAudio/audio.js
@@ -1,4 +1,3 @@
-var APIkey = 'AIzaSyDGIo6ylsqkLqOt1skx2gjOeLI57DPaFz8';
 
 $(document).ready(function(){
     
@@ -7,7 +6,8 @@ $(document).ready(function(){
     });
     
     $("#myModal").on("hide.bs.modal", () => {
-        
+        document.getElementById('myAudio').pause();
+        listaAudio[currentAudio][1] = document.getElementById('myAudio').currentTime;
     });
     
     $('#button1').click(ResearchAudios);
@@ -21,34 +21,58 @@ $(document).ready(function(){
         };
    
     });
+
+    var audio = document.getElementById('myAudio');
+    audio.addEventListener("ended", function(){
+        listaAudio[currentAudio][1] = 0;
+        this.play();
+    }, true);
 });
+
+var listaAudio = '';
 
 function ResearchAudios(){
     
+    $(".list-group").empty();
+    listaAudio = new Array();
     let endpoint = 'https://itunes.apple.com/search?term=' + $('#search').val();
 	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = GestisciItems;
-	xhr.open('get', endpoint, true);
-	xhr.send();
+    xhr.onreadystatechange = GestisciItems;
+    xhr.open('get', endpoint, true);
+    xhr.send();
 		
 	function GestisciItems(e){
 		if (e.target.readyState == 4 && e.target.status == 200){
 				var data = JSON.parse(e.target.responseText);
                 console.log(data);
                 for (var i=0; i<data.resultCount; i++){
+                    if (data.results[i].wrapperType != "track")
+                        continue;
                     $("<li class='list-group-item'><img src=" + data.results[i].artworkUrl100
-                                + "> <a href='#' id='' onclick=''>"
+                                + "> <a href='#' id=" + i + " onclick='return playAudio(this.id);'>"
                                 + "<font color = 'black'>"
-                                + data.results[i].artistName + "</font>" +"</a>").appendTo("#results");
+                                + data.results[i].artistName + " - " + data.results[i].trackCensoredName 
+                                + "</font>" +"</a>").appendTo("#results");
+                    listaAudio[i] = [data.results[i].previewUrl, 0];
                 }
         }
         else{
-            console.log(e.target.statusText)
+            console.log(e.target.statusText);
         }
 	};
 };
 
-  
+var currentAudio = 0;
+
+function playAudio(pos){
+    currentAudio = parseInt(pos);
+    $('#myModal').modal("show");
+    document.getElementById('myAudio').setAttribute('src', listaAudio[pos][0] + '#t=' + listaAudio[pos][1]);
+    var volume = document.getElementById('myAudio').volume;
+    document.getElementById('myAudio').volume = volume/2;
+    document.getElementById('myAudio').play();
+}
+
 function RefreshSearch(){
     $(".list-group").empty();
     $('#search').val('');
